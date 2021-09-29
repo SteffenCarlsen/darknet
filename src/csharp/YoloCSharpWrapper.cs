6e2ac2916e3b85b83359e58e7fb15b315d100631
TaF <bstaffe@gmail.com>
2021-09-29 18:15:59 +0200

    Change to using unsafe code instead of marshalling

--- src/csharp/YoloCSharpWrapper.cs
+++ src/csharp/YoloCSharpWrapper.cs
@@ -8,6 +8,16 @@ namespace Darknet
         private const string YoloLibraryName = "darknet.dll";
         private const int MaxObjects = 1000;
 
+        public YoloWrapper(string configurationFilename, string weightsFilename, int gpu)
+        {
+            InitializeYolo(configurationFilename, weightsFilename, gpu);
+        }
+
+        public void Dispose()
+        {
+            DisposeYolo();
+        }
+
         [DllImport(YoloLibraryName, EntryPoint = "init")]
         private static extern int InitializeYolo(string configurationFilename, string weightsFilename, int gpu);
 
@@ -20,70 +30,42 @@ namespace Darknet
         [DllImport(YoloLibraryName, EntryPoint = "dispose")]
         private static extern int DisposeYolo();
 
+        public bbox_t[] Detect(string filename)
+        {
+            var container = new BboxContainer();
+            var count = DetectImage(filename, ref container);
+            return count == -1 ? null : container.candidates;
+        }
+
+        public unsafe bbox_t[] Detect(byte[] imageData)
+        {
+            var container = new BboxContainer();
+            var count = 0;
+
+            fixed (byte* pointer = imageData)
+            {
+                count = DetectImage((IntPtr)pointer, imageData.Length, ref container);
+            }
+
+            return count == -1 ? null : container.candidates;
+        }
+
         [StructLayout(LayoutKind.Sequential)]
         public struct bbox_t
         {
-            public UInt32 x, y, w, h;    // (x,y) - top-left corner, (w, h) - width & height of bounded box
-            public float prob;           // confidence - probability that the object was found correctly
-            public UInt32 obj_id;        // class of object - from range [0, classes-1]
-            public UInt32 track_id;      // tracking id for video (0 - untracked, 1 - inf - tracked object)
-            public UInt32 frames_counter;
-            public float x_3d, y_3d, z_3d;  // 3-D coordinates, if there is used 3D-stereo camera
-        };
+            public uint x, y, w, h; // (x,y) - top-left corner, (w, h) - width & height of bounded box
+            public float prob; // confidence - probability that the object was found correctly
+            public uint obj_id; // class of object - from range [0, classes-1]
+            public uint track_id; // tracking id for video (0 - untracked, 1 - inf - tracked object)
+            public uint frames_counter;
+            public float x_3d, y_3d, z_3d; // 3-D coordinates, if there is used 3D-stereo camera
+        }
 
         [StructLayout(LayoutKind.Sequential)]
         public struct BboxContainer
         {
             [MarshalAs(UnmanagedType.ByValArray, SizeConst = MaxObjects)]
             public bbox_t[] candidates;
-        }
-
-        public YoloWrapper(string configurationFilename, string weightsFilename, int gpu)
-        {
-            InitializeYolo(configurationFilename, weightsFilename, gpu);
-        }
-
-        public void Dispose()
-        {
-            DisposeYolo();
-        }
-
-        public bbox_t[] Detect(string filename)
-        {
-            var container = new BboxContainer();
-            var count = DetectImage(filename, ref container);
-
-            return container.candidates;
-        }
-
-        public bbox_t[] Detect(byte[] imageData)
-        {
-            var container = new BboxContainer();
-
-            var size = Marshal.SizeOf(imageData[0]) * imageData.Length;
-            var pnt = Marshal.AllocHGlobal(size);
-
-            try
-            {
-                // Copy the array to unmanaged memory.
-                Marshal.Copy(imageData, 0, pnt, imageData.Length);
-                var count = DetectImage(pnt, imageData.Length, ref container);
-                if (count == -1)
-                {
-                    throw new NotSupportedException($"{YoloLibraryName} has no OpenCV support");
-                }
-            }
-            catch (Exception exception)
-            {
-                return null;
-            }
-            finally
-            {
-                // Free the unmanaged memory.
-                Marshal.FreeHGlobal(pnt);
-            }
-
-            return container.candidates;
         }
     }
 }
